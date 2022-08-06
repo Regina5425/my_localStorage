b6ae6819a6ab35e7aa2460ce3d937229d3e0c041
Regina <reina5425@gmail.com>
2022-08-06 20:02:24 +0300

    init new project

---
+++ index.js
@@ -0,0 +1,43 @@
+const button = document.querySelector('button');
+const notes = document.querySelector('.notes');
+const textarea = document.querySelector('textarea');
+
+let arrayNotes = [];
+
+button.addEventListener('click', () => {
+	let text = textarea.value; // находим значение в textarea
+
+	arrayNotes.push(text); // добавляем значение в массив заметок
+
+	let localNotes = JSON.stringify(arrayNotes);  // собираем многострочные заметки
+	localStorage.setItem('myNotes', localNotes);  // установка ключа хранилища с массивом значений
+
+	console.log(JSON.parse(localStorage.getItem('myNotes')));
+
+	textarea.value = '';
+	notes.innerHTML = '';
+
+	render(notes, arrayNotes);
+});
+
+function render(parentNode, data) {
+	for(let i = 0; i < data.length; i++) {
+		let item = data[i];
+
+		const node = document.createElement('div'); // создаем div где лежит одна заметка
+		node.classList.add('notes__added');  // добавляем класс для стилизации
+		node.textContent = item;
+
+		const delBtn = document.createElement('button');
+		delBtn.classList.add('del__btn');
+		delBtn.textContent = 'Удалить';
+
+		delBtn.addEventListener('click', () => {
+			node.remove();
+			data.splice(i, 1);
+		});
+
+		parentNode.append(node);
+		node.append(delBtn);
+	}
+}
